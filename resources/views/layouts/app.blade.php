ae149d5f621cc88d37536e3629304273dcefa5d1
Mathieu SAMARIA <mathieusamaria974@gmail.com>
2022-05-27 09:52:19 +0400

    création de bdd avec les clé etrangere

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    @include('includes.head')
+</head>
+
+<body>
+
+    <header class="text-gray-600 body-font">
+        @include('includes.header')
+    </header>
+
+    <main>
+        @yield('main')
+    </main>
+
+    <footer class="text-gray-600 body-font">
+        @include('includes.footer')
+    </footer>
+
+</body>
+
+</html>l
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/films', function () {
+    return view('films');
+});
